c091199e6f6365511a3441b68f68573c9312b1ac
Maxim Krizhanovski <maxim.krizhanovski@toptal.com>
2019-01-05 01:34:21 +0200

    Add cops for RuboCop

---
+++ lib/database_validations/rubocop/cops.rb
@@ -0,0 +1,2 @@
+require 'database_validations/rubocop/cop/belongs_to'
+require 'database_validations/rubocop/cop/uniqueness_of'
---
+++ spec/rubocop/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rubocop'
+require 'rubocop/rspec/support'
+require 'database_validations/rubocop/cops'
+
+RSpec.shared_context 'rubocop config', :rubocop_config do
+  let(:config) { RuboCop::Config.new(described_class.cop_name => cop_config) }
+end
+
+RSpec.configure do |config|
+  config.include RuboCop::RSpec::ExpectOffense
+end
