2b61913ed8be8e10502b7f7611c8a5896e162283
Maxim Krizhanovski <maxim.krizhanovski@toptal.com>
2019-01-09 19:12:37 +0200

    Handle validates_uniqueness_of in the UniqunessOf cop

--- lib/database_validations/rubocop/cop/uniqueness_of.rb
+++ lib/database_validations/rubocop/cop/uniqueness_of.rb
@@ -7,32 +7,36 @@ module RuboCop
       #   # bad
       #   validates :slug, uniqueness: true
       #   validates :address, uniqueness: { scope: :user_id }
+      #   validates_uniqueness_of :title
       #
       #   # good
       #   validates_db_uniqueness_of :slug
       #   validates_db_uniqueness_of :address, scope: :user_id
+      #   validates_db_uniqueness_of :title
       #
       class UniquenessOf < Cop
         MSG = 'Use `validates_db_uniqueness_of`.'.freeze
 
-        def_node_matcher :uniquness_validation?, '(pair (sym :uniqueness) $_)'
+        def_node_matcher :uniquness_validation?, '(pair (sym :uniqueness) _)'
 
         def on_send(node)
-          return unless node.method_name == :validates
-
-          uniqueness(node) do |option|
-            add_offense(option)
+          if node.method_name == :validates_uniqueness_of
+            add_offense(node, location: :selector)
+          elsif node.method_name == :validates
+            uniqueness(node) do |option|
+              add_offense(option)
+            end
           end
         end
 
         private
 
         def uniqueness(node)
-          options = node.arguments.last
+          options = node.last_argument
           return unless options.hash_type?
 
-          options.each_child_node do |child|
-            yield child if uniquness_validation?(child)
+          options.each_child_node(:pair) do |pair|
+            yield pair if uniquness_validation?(pair)
           end
         end
       end
