a1373b1df138c7b96cd4cc97de67207e446a8523
djezzzl <lawliet.djez@gmail.com>
2019-02-13 09:49:37 +0200

    Generate both keys for uniqueness options

--- lib/database_validations/lib/options_storage.rb
+++ lib/database_validations/lib/options_storage.rb
@@ -7,12 +7,14 @@ module DatabaseValidations
 
     def push_uniqueness(field, options)
       uniqueness_options = UniquenessOptions.new(field, options, adapter)
-      storage[uniqueness_options.key] = uniqueness_options
+      storage[uniqueness_options.index_key] = uniqueness_options
+      storage[uniqueness_options.column_key] = uniqueness_options
     end
 
     def push_belongs_to(field, relation)
       belongs_to_options = BelongsToOptions.new(field, relation, adapter)
-      storage[belongs_to_options.key] = belongs_to_options
+      storage[belongs_to_options.index_key] = belongs_to_options
+      storage[belongs_to_options.column_key] = belongs_to_options
     end
 
     def [](key)
